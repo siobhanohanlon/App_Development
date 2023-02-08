9beba28fd57818e59da94bbe71053224c51ec60f
Siobhán O'Hanlon <80902477+siobhanohanlon@users.noreply.github.com>
2023-02-08 14:13:18 +0000

    Angular

---
+++ Lab3- Angular Intro/Lab3/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root-directive',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'my-first-app';
+  name:string = "Martin";
+  age:number = 22;
+}
